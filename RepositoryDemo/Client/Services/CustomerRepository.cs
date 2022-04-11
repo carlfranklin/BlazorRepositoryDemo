9617d2f4a541b6dc88777574651c129fd482b391
Carl Franklin <carl@franklins.net>
2022-04-11 12:03:30 -0400

    Defaultint to use in-memory repository

--- RepositoryDemo/Client/Services/CustomerRepository.cs
+++ RepositoryDemo/Client/Services/CustomerRepository.cs
@@ -3,9 +3,9 @@
     HttpClient http;
     
     // swap out the controller name
-    //static string controllerName = "inmemorycustomers";
+    static string controllerName = "inmemorycustomers";
     //static string controllerName = "efcustomers";
-    static string controllerName = "dappercustomers";
+    //static string controllerName = "dappercustomers";
 
     public CustomerRepoistory(HttpClient _http)
        : base(_http, controllerName, "Id")

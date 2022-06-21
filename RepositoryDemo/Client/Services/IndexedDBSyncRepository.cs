e5fa93494b550801db536c5d87b40f3bfacb6f25
Carl Franklin <carl@appvnext.com>
2022-06-21 14:11:23 -0400

    oops. One more fix

--- RepositoryDemo/Client/Services/IndexedDBSyncRepository.cs
+++ RepositoryDemo/Client/Services/IndexedDBSyncRepository.cs
@@ -494,7 +494,7 @@ public class IndexedDBSyncRepository<TEntity> : IRepository<TEntity>
             var Id = primaryKey.GetValue(returnValue);
             returnValue = await UpdateKeyToLocal(returnValue);
             await UpdateOfflineAsync(returnValue);
-            await hubConnection.InvokeAsync("SyncRecord", storeName, "update", onlineId.ToString());
+            await hubConnection.InvokeAsync("SyncRecord", storeName, "update", Id.ToString());
         }
         else
         {
